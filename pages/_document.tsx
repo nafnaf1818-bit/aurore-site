46ef9ea4a74837d42bcf6c72c0dde7a9e01fc967
Aurore Michaud <nafnaf1818@gmail.com>
2026-06-15 15:47:05 +0200

    Ajout _document.tsx favicon

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="fr">
+      <Head>
+        <link rel="icon" type="image/png" href="/logo-dauphin.png" />
+        <link rel="apple-touch-icon" href="/logo-dauphin.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
