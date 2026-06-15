c8c77e005eda953eefaeac8d283870790ce3eca7
Aurore Michaud <nafnaf1818@gmail.com>
2026-06-15 18:55:18 +0200

    favicon via app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" type="image/png" href="/logo-dauphin.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
